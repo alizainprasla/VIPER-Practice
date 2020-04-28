2fe259384a03ac257428d6353e6b8238dc0e508c
Ali Zain <azain@foree.co>
2020-04-29 04:32:44 +0500

    Initial Project

---
+++ VIPER-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER-Practice
+//
+//  Created by Ali Zain on 29/04/2020.
+//  Copyright © 2020 Ali Zain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
